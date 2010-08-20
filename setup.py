cb0245aa10a97eb780c88cd14dce0acede82d402
Mark Nottingham <mnot@mnot.net>
2010-08-20 14:22:28 +1000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='redbot',
+      version='1.0',
+      description='Resource Expert Droid',
+      author='Mark Nottingham',
+      author_email='mnot@mnot.net',
+      url='http://redbot.org/project/',
+      packages=['redbot'],
+      package_dir={'redbot': 'src'},
+      scripts=['scripts/redbot'],
+)
