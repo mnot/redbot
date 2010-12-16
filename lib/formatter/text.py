56a90e9598c6387250c63ae2c144f28ca51a57d2
Wim Leers <work@wimleers.com>
2010-12-16 12:18:16 +0100

    Added a --assets parameter (which maps to "descend" internally), and added the ability for the TextFormatter to list multiple HTTP responses.

--- lib/formatter/text.py
+++ lib/formatter/text.py
@@ -27,6 +27,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+import operator
+
 import nbhttp.error as nberror
 import redbot.speak as rs
 
@@ -36,16 +38,23 @@ nl = u"\n"
 
 # TODO: errors and status on stderr with CLI?
 
-class TextFormatter(Formatter):
+class BaseTextFormatter(Formatter):
     """
-    Format a RED object as text.
-    """
+    Base class for text formatters."""
     name = "txt"
     media_type = "text/plain"
 
     msg_categories = [
         rs.c.GENERAL, rs.c.CONNECTION, rs.c.CONNEG, 
         rs.c.CACHING, rs.c.VALIDATION, rs.c.RANGE
+    ]
+
+    link_order = [
+          ('link', 'Head Links'),
+          ('script', 'Script Links'),
+          ('frame', 'Frame Links'),
+          ('iframe', 'IFrame Links'),
+          ('img', 'Image Links'),
     ]
 
     error_template = "Error: %s\n"
@@ -55,7 +64,7 @@ class TextFormatter(Formatter):
 
     def start_output(self):
         pass
-        
+
     def feed(self, red, chunk):
         pass
 
@@ -81,7 +90,6 @@ class TextFormatter(Formatter):
                     red.res_error.get('detail', '')[:20])
             else:
                 raise AssertionError, "Unidentified incomplete response error."
-
 
     def format_headers(self, red):
         out = [u"HTTP/%s %s %s" % (
@@ -133,5 +141,53 @@ class TextFormatter(Formatter):
             color_start = u"\033[1;33m"
             color_end   = u"\033[0;39m"
 
+        if level == "uri":
+            color_start = u"\033[1;34m"
+            color_end   = u"\033[0;39m"
+
         return color_start + string + color_end
-        
+
+
+
+class TextFormatter(BaseTextFormatter):
+    """
+    Format a RED object as text.
+    """
+    name = "txt"
+    media_type = "text/plain"
+
+    def __init__(self, *args, **kw):
+        BaseTextFormatter.__init__(self, *args, **kw)
+
+
+
+class TextListFormatter(BaseTextFormatter):
+    """
+    Format multiple RED responses as a textual list.
+    """
+    name = "txt"
+    media_type = "text/plain"
+    can_multiple = True
+
+
+    def __init__(self, *args, **kw):
+        BaseTextFormatter.__init__(self, *args, **kw)
+
+    def start_output(self):
+        pass
+
+    def finish_output(self, red):
+        "Fill in the template with RED's results."
+        for hdr_tag, heading in self.link_order:
+            droids = [d[0] for d in red.link_droids if d[1] == hdr_tag]
+            self.output("%s\n%s (%d)\n%s\n" % ("="*80, heading, len(droids), "="*80))
+            if droids:
+                droids.sort(key=operator.attrgetter('uri'))
+                for droid in droids:
+                    self.output(self.format_uri(droid) + nl + nl)
+                    self.output(self.format_headers(droid) + nl + nl)
+                    self.output(self.format_recommendations(droid) + nl + nl)
+
+
+    def format_uri(self, red):
+        return self.colorize("uri", red.uri)
