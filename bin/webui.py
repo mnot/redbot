7d04264a94ff597e20f73914c92b574dabf4bacf
Mark Nottingham <mnot@mnot.net>
2011-12-08 19:56:21 +1100

    add pydoc import

--- bin/webui.py
+++ bin/webui.py
@@ -322,7 +322,7 @@ def except_handler_factory(out=None):
         """
         if not etype or not evalue or not etb:
             etype, evalue, etb = sys.exc_info()
-        import cgitb
+        import cgitb, pydoc
         pydoc.html._repr_instance.maxstring = 150
         out(cgitb.reset())
         if logdir is None:
