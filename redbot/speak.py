eb70dfc9db0a7c1565e241dfa09e0dcdb1bc9162
Mark Nottingham <mnot@mnot.net>
2016-09-09 10:59:57 +1000

    remove unused note

--- redbot/speak.py
+++ redbot/speak.py
@@ -81,65 +81,25 @@ response = {
     'range': 'The partial response',
 }
 
+# common notes
 
-class REQUEST_HDR_IN_RESPONSE(Note):
-    category = categories.GENERAL
-    level = levels.BAD
-    summary = u'"%(field_name)s" is a request header.'
-    text = u"""\
-%(field_name)s is only defined to have meaning in requests; in responses, it doesn't have any
-meaning, so RED has ignored it."""
-
-class RESPONSE_HDR_IN_REQUEST(Note):
-    category = categories.GENERAL
-    level = levels.BAD
-    summary = u'"%(field_name)s" is a request header.'
-    text = u"""\
-%(field_name)s is only defined to have meaning in responses; in requests, it doesn't have any
-meaning, so RED has ignored it."""
-
-class HEADER_BLOCK_TOO_LARGE(Note):
-    category = categories.GENERAL
-    level = levels.BAD
-    summary = u"%(response)s's headers are very large (%(header_block_size)s)."
-    text = u"""\
-Some implementations have limits on the total size of headers that they'll accept. For example,
-Squid's default configuration limits header blocks to 20k."""
-
-class HEADER_TOO_LARGE(Note):
-    category = categories.GENERAL
+class MISSING_HDRS_304(Note):
+    category = categories.VALIDATION
     level = levels.WARN
-    summary = u"The %(header_name)s header is very large (%(header_size)s)."
+    summary = u"The %(subreq_type)s response is missing required headers."
     text = u"""\
-Some implementations limit the size of any single header line."""
+HTTP requires `304 Not Modified` responses to have certain headers, if they are also present in a
+normal (e.g., `200 OK` response).
 
-class HEADER_NAME_ENCODING(Note):
-    category = categories.GENERAL
-    level = levels.BAD
-    summary = u"The %(header_name)s header's name contains non-ASCII characters."
-    text = u"""\
-HTTP header field-names can only contain ASCII characters. RED has detected (and possibly removed)
-non-ASCII characters in this header name."""
+%(response)s is missing the following headers: `%(missing_hdrs)s`.
 
-class HEADER_VALUE_ENCODING(Note):
-    category = categories.GENERAL
-    level = levels.WARN
-    summary = u"The %(header_name)s header's value contains non-ASCII characters."
-    text = u"""\
-HTTP headers use the ISO-8859-1 character set, but in most cases are pure ASCII (a subset of this
-encoding).
+This can affect cache operation; because the headers are missing, caches might remove them from
+their cached copies."""
 
-This header has non-ASCII characters, which RED has interpreted as being encoded in
-ISO-8859-1. If another encoding is used (e.g., UTF-8), the results may be unpredictable."""
 
-class HEADER_DEPRECATED(Note):
-    category = categories.GENERAL
-    level = levels.WARN
-    summary = u"The %(header_name)s header is deprecated."
-    text = u"""\
-This header field is no longer recommended for use, because of interoperability problems and/or
-lack of use. See [the deprecation notice](%(deprecation_ref)s) for more information."""
 
+
+# fetch notes
 
 class BODY_NOT_ALLOWED(Note):
     category = categories.CONNECTION
@@ -151,31 +111,6 @@ HTTP defines a few special situations where a response does not allow a body. Th
 
 %(response)s had a body, despite it being disallowed. Clients receiving it may treat the body as
 the next response in the connection, leading to interoperability and security issues."""
-
-
-# Specific headers
-
-class BAD_CC_SYNTAX(Note):
-    category = categories.CACHING
-    level = levels.BAD
-    summary = u"The %(bad_cc_attr)s Cache-Control directive's syntax is incorrect."
-    text = u"This value must be an integer."
-
-class AGE_NOT_INT(Note):
-    category = categories.CACHING
-    level = levels.BAD
-    summary = u"The Age header's value should be an integer."
-    text = u"""\
-The `Age` header indicates the age of the response; i.e., how long it has been cached
-since it was generated. The value given was not an integer, so it is not a valid age."""
-
-class AGE_NEGATIVE(Note):
-    category = categories.CACHING
-    level = levels.BAD
-    summary = u"The Age headers' value must be a positive integer."
-    text = u"""\
-The `Age` header indicates the age of the response; i.e., how long it has been cached
-since it was generated. The value given was negative, so it is not a valid age."""
 
 class BAD_CHUNK(Note):
     category = categories.CONNECTION
@@ -200,18 +135,6 @@ This issue is often caused by sending an integer chunk size instead of one in he
 `Transfer-Encoding: chunked` without actually chunking the response body."""
 
 
-class MISSING_HDRS_304(Note):
-    category = categories.VALIDATION
-    level = levels.WARN
-    summary = u"The %(subreq_type)s response is missing required headers."
-    text = u"""\
-HTTP requires `304 Not Modified` responses to have certain headers, if they are also present in a
-normal (e.g., `200 OK` response).
-
-%(response)s is missing the following headers: `%(missing_hdrs)s`.
-
-This can affect cache operation; because the headers are missing, caches might remove them from
-their cached copies."""
 
 
 if __name__ == '__main__':
