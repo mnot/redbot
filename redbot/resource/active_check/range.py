ed460e716d742911ba3554b5c85aa9c1477bdb64
Mark Nottingham <mnot@mnot.net>
2016-09-26 18:09:03 +1000

    missing headers doesn't depend upon if-range

--- redbot/resource/active_check/range.py
+++ redbot/resource/active_check/range.py
@@ -58,11 +58,9 @@ class RangeRequest(SubRequest):
                'gzip' not in self.response.parsed_headers.get(c_e, []):
                 self.add_base_note('header-accept-ranges header-content-encoding',
                                    RANGE_NEG_MISMATCH)
-                return
-            if not [True for h in self.base.request.headers if h[0].lower() == 'if-range']:
-                self.check_missing_hdrs([
-                    'date', 'cache-control', 'content-location', 'etag',
-                    'expires', 'vary'], MISSING_HDRS_206)
+                return            
+            self.check_missing_hdrs(['date', 'cache-control', 'content-location', 'etag',
+                                    'expires', 'vary'], MISSING_HDRS_206)
             if self.response.parsed_headers.get('etag', 1) == \
               self.base.response.parsed_headers.get('etag', 2):
                 if self.response.payload == self.range_target:
