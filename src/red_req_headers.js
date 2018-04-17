68128dbf30339c33966371770cabcef19099a6a8
Mark Nottingham <mnot@mnot.net>
2018-04-17 17:44:55 +1000

    lint with standard.js

--- src/red_req_headers.js
+++ src/red_req_headers.js
@@ -1,184 +1,182 @@
 
 /* request headers */
 
+/* global jQuery, $, config, alert */
 
-var known_req_hdrs = {
+var knownReqHdrs = {
   'Accept-Language': ['', 'en', 'en-us', 'en-uk', 'fr'],
   'Cache-Control': ['', 'no-cache', 'only-if-cached'],
   'Cookie': null,
   'Referer': null,
-  'User-Agent': [ 
-    "RED/" + config.redbot_version + " (http://redbot.org/about)",
-    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; )",
-    "Mozilla/5.0 (X11; U; Linux x86_64; en-US) Gecko Firefox/3.0.8",
-    "Opera/9.80 (Windows NT 5.1; U; cs) Presto/2.2.15 Version/10.00"
+  'User-Agent': [
+    'RED/' + config.redbot_version + ' (http://redbot.org/about)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; )',
+    'Mozilla/5.0 (X11; U; Linux x86_64; en-US) Gecko Firefox/3.0.8',
+    'Opera/9.80 (Windows NT 5.1; U; cs) Presto/2.2.15 Version/10.00'
   ]
-};
+}
 
-var red_req_hdrs = [
+var redReqHdrs = [
   'accept-encoding',
   'if-modified-since',
   'if-none-match',
   'connection',
   'transfer-encoding',
   'content-length'
-];
+]
 
-
-
-function add_req_hdr(hdr_name, hdr_val){
-  var hdr_shell = "<div class='req_hdr'>";
-  hdr_shell += "<a href='#' class='delete_req_hdr'>x</a> ";
-  hdr_shell += "<span class='hdr_name'></span>";
-  hdr_shell += ": <span class='hdr_val'></span>";
-  hdr_shell += "<input type='hidden' name='req_hdr' value=''/>";
-  hdr_shell += "</div>";
-  $("#req_hdrs").append(hdr_shell);
-  var req_hdr = $("#req_hdrs > .req_hdr:last");
+function addReqHdr (hdrName, hdrVal) {
+  var hdrShell = "<div class='req_hdr'>"
+  hdrShell += "<a href='#' class='delete_req_hdr'>x</a> "
+  hdrShell += "<span class='hdr_name'></span>"
+  hdrShell += ": <span class='hdr_val'></span>"
+  hdrShell += "<input type='hidden' name='req_hdr' value=''/>"
+  hdrShell += '</div>'
+  $('#req_hdrs').append(hdrShell)
+  var reqHdr = $('#req_hdrs > .req_hdr:last')
 
   /* populate header name list */
-  if (hdr_name == null || hdr_name in known_req_hdrs) {
-    var name_html = "<select class='hdr_name'><option/>";
-    for (name in known_req_hdrs) {
-      if (name == hdr_name) {
-        name_html += "<option selected='true'>" + name + "</option>";
+  if (hdrName == null || hdrName in knownReqHdrs) {
+    var nameHtml = "<select class='hdr_name'><option/>"
+    for (var name in knownReqHdrs) {
+      if (name === hdrName) {
+        nameHtml += "<option selected='true'>" + name + '</option>'
       } else {
-        name_html += "<option>" + name + "</option>";
+        nameHtml += '<option>' + name + '</option>'
       }
     }
-    name_html += "<option value='other...'>other...</option>";
-    name_html += "</select>";
-    $(".hdr_name", req_hdr).replaceWith(name_html);
+    nameHtml += "<option value='other...'>other...</option>"
+    nameHtml += '</select>'
+    $('.hdr_name', reqHdr).replaceWith(nameHtml)
 
     /* select specified header, if any */
-    if (hdr_name != null) {
-      known_hdr_vals = known_req_hdrs[hdr_name];
-      if (known_hdr_vals == null) {
-        $(".hdr_val", req_hdr)
-         .replaceWith('<input class="hdr_val" type="text"/>');
-        $(".hdr_val", req_hdr).val(hdr_val);
-      } else if (jQuery.inArray(hdr_val, known_hdr_vals) > -1) {
-        var val_html = "<select class='hdr_val'>";
-        val_html += "<option />";
-        for (i in known_hdr_vals) {
-          var val = known_hdr_vals[i];
-          if (hdr_val == val) {
-            val_html += "<option selected='true'>" + val + "</option>";
+    if (hdrName != null) {
+      var knownHdrVals = knownReqHdrs[hdrName]
+      if (knownHdrVals == null) {
+        $('.hdr_val', reqHdr)
+          .replaceWith('<input class="hdrVal" type="text"/>')
+        $('.hdr_val', reqHdr).val(hdrVal)
+      } else if (jQuery.inArray(hdrVal, knownHdrVals) > -1) {
+        var valHtml = "<select class='hdr_val'>"
+        valHtml += '<option />'
+        for (var i in knownHdrVals) {
+          var val = knownHdrVals[i]
+          if (hdrVal === val) {
+            valHtml += "<option selected='true'>" + val + '</option>'
           } else {
-            val_html += "<option>" + val + "</option>";
+            valHtml += '<option>' + val + '</option>'
           }
         }
-        val_html += "<option value='other...'>other...</option>";
-        val_html += "</select>";
-        $(".hdr_val", req_hdr).replaceWith(val_html);
-      } else if (hdr_val != null) {
-        $(".hdr_val", req_hdr)
-         .replaceWith('<input class="hdr_val" type="text"/>');
-        $(".hdr_val", req_hdr).val(hdr_val);
+        valHtml += "<option value='other...'>other...</option>"
+        valHtml += '</select>'
+        $('.hdr_val', reqHdr).replaceWith(valHtml)
+      } else if (hdrVal != null) {
+        $('.hdr_val', reqHdr)
+          .replaceWith('<input class="hdrVal" type="text"/>')
+        $('.hdr_val', reqHdr).val(hdrVal)
       }
-      $("input[type=hidden]", req_hdr).val(hdr_name + ":" + hdr_val);
+      $('input[type=hidden]', reqHdr).val(hdrName + ':' + hdrVal)
     }
   } else {
-    $(".hdr_name", req_hdr)
-     .replaceWith('<input class="hdr_name" type="text"/>');
-    $(".hdr_name", req_hdr).val(hdr_name);
-    $(".hdr_val", req_hdr)
-     .replaceWith('<input class="hdr_val" type="text"/>');
-    $(".hdr_val", req_hdr).val(hdr_val);
-    $("input[type=hidden]", req_hdr).val(hdr_name + ":" + hdr_val);
+    $('.hdr_name', reqHdr)
+      .replaceWith('<input class="hdr_name" type="text"/>')
+    $('.hdr_name', reqHdr).val(hdrName)
+    $('.hdr_val', reqHdr)
+      .replaceWith('<input class="hdr_val" type="text"/>')
+    $('.hdr_val', reqHdr).val(hdrVal)
+    $('input[type=hidden]', reqHdr).val(hdrName + ':' + hdrVal)
   }
 }
 
-
-$(document).ready(function() {
-  
+$(document).ready(function () {
   /* add pre-populated headers */
-  for (i in config.redbot_req_hdrs) {
-    var hdr = config.redbot_req_hdrs[i];
-    add_req_hdr(hdr[0], hdr[1]);
+  for (var i in config.redbot_reqHdrs) {
+    var hdr = config.redbot_reqHdrs[i]
+    addReqHdr(hdr[0], hdr[1])
   }
-  
+
   /* handle the add header button */
-  $("#add_req_hdr").click(function(){
-    add_req_hdr();
-    return false;
-  });
-    
+  $('#add_req_hdr').click(function () {
+    addReqHdr()
+    return false
+  })
+
   /* handle the delete header button */
-  $("#req_hdrs").on("click", ".delete_req_hdr", function(ev){
-    var req_hdr = $(ev.target).closest(".req_hdr");
-    req_hdr.remove();
-    return false;
-  });    
+  $('#req_hdrs').on('click', '.delete_req_hdr', function (ev) {
+    var reqHdr = $(ev.target).closest('.req_hdr')
+    reqHdr.remove()
+    return false
+  })
 
   /* handle header name list changes */
-  $("#req_hdrs").on("change", "select.hdr_name", function(ev) {
-    var req_hdr = $(ev.target).closest(".req_hdr");
-    var hdr_name = $(".hdr_name > option:selected", req_hdr).val()
-    if (hdr_name in known_req_hdrs) {
-      if (known_req_hdrs[hdr_name] == null) {
-        hdr_val = "<input class='hdr_val' type='text'/>";
-        $(".hdr_val", req_hdr).replaceWith(hdr_val);
+  $('#req_hdrs').on('change', 'select.hdr_name', function (ev) {
+    var reqHdr = $(ev.target).closest('.req_hdr')
+    var hdrName = $('.hdr_name > option:selected', reqHdr).val()
+    if (hdrName in knownReqHdrs) {
+      var hdrVal
+      if (knownReqHdrs[hdrName] == null) {
+        hdrVal = "<input class='hdr_val' type='text'/>"
+        $('.hdr_val', reqHdr).replaceWith(hdrVal)
       } else {
-        hdr_val = "<select class='hdr_val'>";
-        for (val in known_req_hdrs[hdr_name]) {
-          hdr_val += "<option>" + known_req_hdrs[hdr_name][val] + "</option>";
+        hdrVal = "<select class='hdr_val'>"
+        for (var val in knownReqHdrs[hdrName]) {
+          hdrVal += '<option>' + knownReqHdrs[hdrName][val] + '</option>'
         }
-        hdr_val += "<option value='other...'>other...</option>";
-        hdr_val += "</select>";
-        $(".hdr_val", req_hdr).replaceWith(hdr_val);
+        hdrVal += "<option value='other...'>other...</option>"
+        hdrVal += '</select>'
+        $('.hdr_val', reqHdr).replaceWith(hdrVal)
       }
-    } else if (hdr_name == "other...") {
-      $(".hdr_name", req_hdr)
-       .replaceWith("<input class='hdr_name' type='text'/>");
-      $(".hdr_val", req_hdr)
-       .replaceWith("<input class='hdr_val' type='text'/>");      
+    } else if (hdrName === 'other...') {
+      $('.hdr_name', reqHdr)
+        .replaceWith("<input class='hdr_name' type='text'/>")
+      $('.hdr_val', reqHdr)
+        .replaceWith("<input class='hdr_val' type='text'/>")
     }
-  });
+  })
 
   /* handle header name text changes */
-  $("#req_hdrs").on("change", "input.hdr_name", function(ev) {
+  $('#req_hdrs').on('change', 'input.hdr_name', function (ev) {
     /* alert on dangerous changes */
-    if (jQuery.inArray(hdr_name.toLowerCase(), red_req_hdrs) > -1) {
-      alert("The " + hdr_name + " request header is used by RED in its tests. Setting it yourself can lead to unpredictable results.");
+    var reqHdr = $(ev.target).closest('.req_hdr')
+    var hdrName = $('.hdr_name', reqHdr).val()
+    if (jQuery.inArray(hdrName.toLowerCase(), redReqHdrs) > -1) {
+      alert('The ' + hdrName + ' request header is used by RED in its tests. Setting it yourself can lead to unpredictable results.')
     }
-    $("input[type=hidden]", req_hdr).val(hdr_name + ":" + "");
-  });
+    $('input[type=hidden]', reqHdr).val(hdrName + ':' + '')
+  })
 
   /* handle header value list changes */
-  $("#req_hdrs").on("change", "select.hdr_val", function(ev) {
-    var req_hdr = $(ev.target).closest(".req_hdr");
-    var hdr_name = $(".hdr_name", req_hdr).val();
-    var hdr_val = "";
-    if ($(".hdr_val > option:selected", req_hdr).val() == "other...") {
-      $(".hdr_val", req_hdr)
-       .replaceWith("<input class='hdr_val' type='text'/>");
+  $('#req_hdrs').on('change', 'select.hdr_val', function (ev) {
+    var reqHdr = $(ev.target).closest('.req_hdr')
+    var hdrName = $('.hdr_name', reqHdr).val()
+    var hdrVal = ''
+    if ($('.hdr_val > option:selected', reqHdr).val() === 'other...') {
+      $('.hdr_val', reqHdr)
+        .replaceWith("<input class='hdr_val' type='text'/>")
     } else {
-      hdr_val = $(".hdr_val > option:selected", req_hdr).val();
+      hdrVal = $('.hdr_val > option:selected', reqHdr).val()
     }
-    $("input[type=hidden]", req_hdr).val(hdr_name + ":" + hdr_val);
-    $("#uri").focus();
-
-  });
+    $('input[type=hidden]', reqHdr).val(hdrName + ':' + hdrVal)
+    $('#uri').focus()
+  })
 
   /* handle header value text changes */
-  $("#req_hdrs").on("change", "input.hdr_val", function(ev) {
-    var req_hdr = $(ev.target).closest(".req_hdr");
-    var hdr_name = $(".hdr_name", req_hdr).val();
-    var hdr_val = $(".hdr_val:text", req_hdr).val();
-    $("input[type=hidden]", req_hdr).val(hdr_name + ":" + hdr_val);
-    $("#uri").focus();
-  });  
-  
-  /* handle return in textbox values */
-  $("#req_hdrs").on("keypress", "input.hdr_val", function (ev) {
-    if (ev.which == 13) {
-      var req_hdr = $(ev.target).closest(".req_hdr");
-      $("input.hdr_val", req_hdr).trigger("change");
-      $("#request_form").submit();
-      return false;
-    }
-  });
+  $('#req_hdrs').on('change', 'input.hdr_val', function (ev) {
+    var reqHdr = $(ev.target).closest('.req_hdr')
+    var hdrName = $('.hdr_name', reqHdr).val()
+    var hdrVal = $('.hdr_val:text', reqHdr).val()
+    $('input[type=hidden]', reqHdr).val(hdrName + ':' + hdrVal)
+    $('#uri').focus()
+  })
 
-});
+  /* handle return in textbox values */
+  $('#req_hdrs').on('keypress', 'input.hdr_val', function (ev) {
+    if (ev.which === 13) {
+      var reqHdr = $(ev.target).closest('.req_hdr')
+      $('input.hdr_val', reqHdr).trigger('change')
+      $('#request_form').submit()
+      return false
+    }
+  })
+})
--- src/red_script.js
+++ src/red_script.js
@@ -1,164 +1,161 @@
 
 /* Configuration */
 
+/* global $, prettyPrint */
 
-var config = JSON.parse($("#config").html());
+var config = JSON.parse($('#config').html()) // eslint-disable-line
 
+$(document).ready(function () {
+  'use strict'
 
-$(document).ready(function() {
-  
-  "use strict";
-  
   /* URI */
 
-  $("input").keypress(function (e) {
-    if (e.which == 13) {
-      $("#request_form").submit();
-      return false;
+  $('input').keypress(function (e) {
+    if (e.which === 13) {
+      $('#request_form').submit()
+      return false
     }
-  });
+  })
 
   /* help */
-  
-  $("#help").click(function () {
-    $(".help").toggle();
-  });
 
-  $(".help").click(function () {
-    $(this).fadeOut();
-  });
+  $('#help').click(function () {
+    $('.help').toggle()
+  })
+
+  $('.help').click(function () {
+    $(this).fadeOut()
+  })
 
   /* single response display */
 
-  var hidden_list = $("#hidden_list");
+  var hiddenList = $('#hidden_list')
 
-  $("span.hdr").hoverPopup(
-    function(e){
-      var name = $(this).attr('data-name');
-      return $("li#header-" + name, hidden_list).html();
+  $('span.hdr').hoverPopup(
+    function (e) {
+      var name = $(this).attr('data-name')
+      return $('li#header-' + name, hiddenList).html()
     },
-    function(e){
-      var name = $(this).attr('data-name');
-      var offset = $(this).attr('data-offset');
-      $("span.hdr[data-name='" + name + "']").addClass('hilight');
-      $("li.note").each(function(index, note) {
-        var note_interesting = false;
-        var subjects = $(note).attr("data-subject").split(" ");
-        for (var i=0; i < subjects.length; i++) {
-          var subject = subjects[i];
-          if (subject == "header-" + name) {
-            note_interesting = true;
-            break;
+    function (e) {
+      var name = $(this).attr('data-name')
+      var offset = $(this).attr('data-offset')
+      $("span.hdr[data-name='" + name + "']").addClass('hilight')
+      $('li.note').each(function (index, note) {
+        var noteInteresting = false
+        var subjects = $(note).attr('data-subject').split(' ')
+        for (var i = 0; i < subjects.length; i++) {
+          var subject = subjects[i]
+          if (subject === 'header-' + name) {
+            noteInteresting = true
+            break
           }
-          if (subject == "offset-" + offset) {
-            note_interesting = true;
-            break;
-          }        
+          if (subject === 'offset-' + offset) {
+            noteInteresting = true
+            break
+          }
         }
-        if (! note_interesting) {
-          $(note).fadeTo(100, 0.15);
+        if (!noteInteresting) {
+          $(note).fadeTo(100, 0.15)
         }
       })
     },
-    function(e){
-      var name = $(this).attr('data-name');
-      $("span.hdr[data-name='" + name + "']").removeClass('hilight');
-      $("li.note").fadeTo(100, 1.0);
+    function (e) {
+      var name = $(this).attr('data-name')
+      $("span.hdr[data-name='" + name + "']").removeClass('hilight')
+      $('li.note').fadeTo(100, 1.0)
     }
-  );
+  )
 
-  function find_header_targets(subjects, cb) {
-    var targets = [];
-    for (var i=0; i < subjects.length; i++) {
-      var subject = subjects[i];
-      var target;
+  function findHeaderTargets (subjects, cb) {
+    var targets = []
+    for (var i = 0; i < subjects.length; i++) {
+      var subject = subjects[i]
+      var target
       if (subject.indexOf('offset-') === 0) {
-        target = $("span.hdr[data-offset='" + subject.slice(7) + "']");
-      }
-      else if (subject.indexOf('header-') === 0) {
-        target = $("span.hdr[data-name='" + subject.slice(7) + "']");
+        target = $("span.hdr[data-offset='" + subject.slice(7) + "']")
+      } else if (subject.indexOf('header-') === 0) {
+        target = $("span.hdr[data-name='" + subject.slice(7) + "']")
       }
       if (target) {
-        cb(target);
-        targets.push(target);
+        cb(target)
+        targets.push(target)
       }
-    }  
+    }
   }
 
-  $("li.note span").hoverPopup(
-    function(e){
-      return $("li#" + $(this).parent().attr('data-name'), hidden_list)
-             .html();
+  $('li.note span').hoverPopup(
+    function (e) {
+      return $('li#' + $(this).parent().attr('data-name'), hiddenList)
+        .html()
     },
-    function(e){
-      find_header_targets(
-        $(this).parent().attr('data-subject').split(" "),
-        function(target) {
-          target.addClass('hilight');
+    function (e) {
+      findHeaderTargets(
+        $(this).parent().attr('data-subject').split(' '),
+        function (target) {
+          target.addClass('hilight')
         }
-      );
+      )
     },
-    function(e){
-      find_header_targets(
-        $(this).parent().attr('data-subject').split(" "),
-        function(target) {
-          target.removeClass('hilight');
+    function (e) {
+      findHeaderTargets(
+        $(this).parent().attr('data-subject').split(' '),
+        function (target) {
+          target.removeClass('hilight')
         }
-      );
+      )
     }
-  );
+  )
 
-  var body_view_clicks = 0;
-  $("#body_view").click(function() {
-    body_view_clicks += 1;
-    if (body_view_clicks % 2 == 1) {
-      $("#details").fadeOut('fast', function() {
-        $("#body").fadeIn('fast');
-        prettyPrint();
-        $("#body_view").text("show notes");
-      });      
+  var bodyViewClicks = 0
+  $('#body_view').click(function () {
+    bodyViewClicks += 1
+    if (bodyViewClicks % 2 === 1) {
+      $('#details').fadeOut('fast', function () {
+        $('#body').fadeIn('fast')
+        prettyPrint()
+        $('#body_view').text('show notes')
+      })
     } else {
-      $("#body").fadeOut('fast', function() {
-        $("#details").fadeIn('fast');
-        $("#body_view").text("show body");
-      });      
+      $('#body').fadeOut('fast', function () {
+        $('#details').fadeIn('fast')
+        $('#body_view').text('show body')
+      })
     }
-    return false;
-  });
-
-  $("#save").click(function() {
-    $("#save_form").submit();
+    return false
   })
 
+  $('#save').click(function () {
+    $('#save_form').submit()
+  })
 
   /* multiple result display */
 
-  $("tr.droid").hoverIntent(function() {
-    var classes = this.className.split(" ");
-    $("li.note").fadeTo(100, 0.15);
-    for (var i=0; i < classes.length; i++) {
-      var c = classes[i];
-      if (c != 'droid') {
-        $("li.note:eq(" + c +")").fadeTo(50, 1.0);
+  var tid = false
+  $('tr.droid').hoverIntent(function () {
+    var classes = this.className.split(' ')
+    $('li.note').fadeTo(100, 0.15)
+    for (var i = 0; i < classes.length; i++) {
+      var c = classes[i]
+      if (c !== 'droid') {
+        $('li.note:eq(' + c + ')').fadeTo(50, 1.0)
       }
     }
     if (tid !== false) {
-      clearTimeout(tid);
-      tid = false;
+      clearTimeout(tid)
+      tid = false
     }
-  }, function(){
-    tid = setTimeout(function() {
-      $("li.note").fadeTo(50, 1.0);
-    }, 100);
-  });
+  }, function () {
+    tid = setTimeout(function () {
+      $('li.note').fadeTo(50, 1.0)
+    }, 100)
+  })
 
-  $("span.prob_num").hoverPopup(function(e) {
-    return $(this).children(".hidden").html();
-  });
+  $('span.prob_num').hoverPopup(function (e) {
+    return $(this).children('.hidden').html()
+  })
 
-  $("a.preview").hoverPopup(function(e) {
-    var link = (this.title != "") ? this.title : this.href;
-    return "<img src='" + link + "'/><br />" + link;
-  });
-
-});
+  $('a.preview').hoverPopup(function (e) {
+    var link = (this.title !== '') ? this.title : this.href
+    return "<img src='" + link + "'/><br />" + link
+  })
+})
