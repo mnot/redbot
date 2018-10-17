cafc3a286ec09356df87ee5dffee45308afb1c65
Mark Nottingham <mnot@mnot.net>
2018-10-17 12:36:52 +1100

    Make typecheck happy

--- bin/redbot_cgi.py
+++ bin/redbot_cgi.py
@@ -4,7 +4,7 @@
 A CGI-based Web UI for RED, the Resource Expert Droid.
 """
 
-from configparser import ConfigParser
+from configparser import ConfigParser, SectionProxy
 import locale
 import os
 import sys
@@ -13,15 +13,15 @@ import thor
 from thor.loop import _loop
 
 from redbot import __version__
+from redbot.type import RawHeaderListType
 from redbot.webui import RedWebUi, except_handler_factory
 
 _loop.precision = .1
 
 
-
-def cgi_main(config):
+def cgi_main(config: SectionProxy) -> None:
     """Run REDbot as a CGI Script."""
-    def out(inbytes):
+    def out(inbytes: bytes) -> None:
         try:
             sys.stdout.buffer.write(inbytes)
             sys.stdout.flush()
@@ -36,7 +36,7 @@ def cgi_main(config):
     method = os.environ.get('REQUEST_METHOD').encode(config['charset'])
     query_string = os.environ.get('QUERY_STRING', "").encode(config['charset'])
 
-    def response_start(code, phrase, res_hdrs):
+    def response_start(code: bytes, phrase: bytes, res_hdrs: RawHeaderListType) -> None:
         out_v = [b"Status: %s %s" % (code, phrase)]
         for name, value in res_hdrs:
             out_v.append(b"%s: %s" % (name, value))
@@ -45,7 +45,7 @@ def cgi_main(config):
         out(b"\n".join(out_v))
 
     freak_ceiling = 20000
-    def response_body(chunk):
+    def response_body(chunk: bytes) -> None:
         rest = None
         if len(chunk) > freak_ceiling:
             rest = chunk[freak_ceiling:]
@@ -54,14 +54,15 @@ def cgi_main(config):
         if rest:
             response_body(rest)
 
-    def response_done(trailers):
+    def response_done(trailers: RawHeaderListType) -> None:
         thor.schedule(0, thor.stop)
 
     try:
-        RedWebUi(config, method, query_string, response_start, response_body, response_done)
+        RedWebUi(config, method.decode(config['charset']), query_string,
+          response_start, response_body, response_done)
         thor.run()
     except Exception:
-        except_handler_factory(config, qs=query_string)()
+        except_handler_factory(config, qs=query_string.decode(config['charset']))()
 
 
 if __name__ == "__main__":
