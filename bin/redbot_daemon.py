3f08f5ff7ddb4b63217e3e432cd4a2fec04f7af7
Mark Nottingham <mnot@mnot.net>
2018-11-14 16:14:09 +1100

    make it an object

--- bin/redbot_daemon.py
+++ bin/redbot_daemon.py
@@ -8,6 +8,7 @@ from configparser import ConfigParser, SectionProxy
 import locale
 import os
 import sys
+from typing import Dict
 from urllib.parse import urlsplit
 
 import thor
@@ -22,49 +23,63 @@ from redbot.type import RawHeaderListType
 from redbot.webui import RedWebUi
 
 _loop.precision = .1
-watchdog_freq = 5
 
 
-def standalone_main(config: SectionProxy) -> None:
+class RedBotServer:
     """Run REDbot as a standalone Web server."""
 
-    # load static files
     static_types = {
         b'.js': b'text/javascript',
         b'.css': b'text/css',
         b'.png': b'image/png',
     }
-    static_files = {}
+    watchdog_freq = 5
 
-    def walk_files(dir_name: str, base: str = "") -> None:
+    def __init__(self, config: SectionProxy) -> None:
+        self.config = config
+        self.static_files = {}    # type: Dict[bytes, bytes]
+        # Load static files
+        self.walk_files(config['asset_dir'], b"static/")
+        if config.get('extra_base_dir'):
+            self.walk_files(config['extra_base_dir'])
+        # Set up the watchdog
+        if os.environ.get("SYSTEMD_WATCHDOG"):
+            thor.schedule(self.watchdog_freq, self.watchdog_ping)
+        # Set up the server
+        server = thor.http.HttpServer(config.get('host', ''), int(config['port']))
+        server.on('exchange', self.red_handler)
+        try:
+            thor.run()
+        except KeyboardInterrupt:
+            sys.stderr.write("Stopping...\n")
+            thor.stop()
+
+    def walk_files(self, dir_name: str, base: bytes = b"") -> None:
         for root, dirs, files in os.walk(dir_name):
             for name in files:
                 try:
                     path = os.path.join(root, name)
-                    uri = os.path.relpath(path, dir_name)
-                    static_files[b"/%s%s" % (base, uri.encode('utf-8'))] = open(path, 'rb').read()
+                    uri = os.path.relpath(path, dir_name).encode('utf-8')
+                    self.static_files[b"/%s%s" % (base, uri)] = open(path, 'rb').read()
                 except IOError:
                     sys.stderr.write("* Problem loading %s\n" % path)
-    walk_files(config['asset_dir'], "static/")
-    if config.get('extra_base_dir'):
-        walk_files(config['extra_base_dir'])
 
-    def red_handler(x: EventEmitter) -> None:
+    def red_handler(self, x: EventEmitter) -> None:
         @thor.events.on(x)
         def request_start(method: bytes, uri: bytes, req_hdrs: RawHeaderListType) -> None:
             p_uri = urlsplit(uri)
-            if p_uri.path in static_files:
+            if p_uri.path in self.static_files:
                 headers = []
                 file_ext = os.path.splitext(p_uri.path)[1].lower()
-                content_encoding = static_types.get(file_ext, b'application/octet-stream')
+                content_encoding = self.static_types.get(file_ext, b'application/octet-stream')
                 headers.append((b'Content-Encoding', content_encoding))
                 headers.append((b'Cache-Control', b'max-age=3600'))
                 x.response_start(b"200", b"OK", headers)
-                x.response_body(static_files[p_uri.path])
+                x.response_body(self.static_files[p_uri.path])
                 x.response_done([])
             elif p_uri.path == b"/":
                 try:
-                    RedWebUi(config, method.decode(config['charset']), p_uri.query,
+                    RedWebUi(self.config, method.decode(self.config['charset']), p_uri.query,
                              x.response_start, x.response_body, x.response_done)
                 except Exception:
                     sys.stderr.write("""
@@ -82,21 +97,10 @@ in standalone server mode. Details follow.
                 x.response_start(b"404", b"Not Found", [])
                 x.response_done([])
 
-    def watchdog_ping() -> None:
+    def watchdog_ping(self) -> None:
         notify(Notification.WATCHDOG)
-        thor.schedule(watchdog_freq, watchdog_ping)
+        thor.schedule(self.watchdog_freq, self.watchdog_ping)
 
-    if os.environ.get("SYSTEMD_WATCHDOG"):
-        thor.schedule(watchdog_freq, watchdog_ping)
-
-    server = thor.http.HttpServer(config.get('host', ''), int(config['port']))
-    server.on('exchange', red_handler)
-
-    try:
-        thor.run()
-    except KeyboardInterrupt:
-        sys.stderr.write("Stopping...\n")
-        thor.stop()
 
 
 if __name__ == "__main__":
@@ -122,4 +126,4 @@ if __name__ == "__main__":
         "Starting on PID %s...\n" % os.getpid() + \
         "http://%s:%s/\n" % (redconf.get('host', ''), redconf['port']))
 
-    standalone_main(redconf)
+    RedBotServer(redconf)
