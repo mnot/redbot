a6d864a03502d8b923bfed5a7544c9ffb8c5b681
Mark Nottingham <mnot@mnot.net>
2018-11-16 12:10:15 +1100

    update thor requirement

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='redbot',
       },
       python_requires=">=3.5",
       install_requires=[
-          'thor >= 0.7.1',
+          'thor >= 0.7.2',
           'markdown >= 2.6.5'
       ],
       extras_require={
