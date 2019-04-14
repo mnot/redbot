51d4154148f74967c49db3d5dd8bbea3ef9feace
Mark Nottingham <mnot@mnot.net>
2019-04-14 17:28:01 +1000

    Move except_handler_factory into cgi
    it's specific to CGI.

--- bin/redbot_cgi.py
+++ bin/redbot_cgi.py
@@ -14,7 +14,7 @@ from thor.loop import _loop
 
 from redbot import __version__
 from redbot.type import RawHeaderListType
-from redbot.webui import RedWebUi, except_handler_factory
+from redbot.webui import RedWebUi
 
 _loop.precision = .1
 
@@ -63,6 +63,79 @@ def cgi_main(config: SectionProxy) -> None:
         thor.run()
     except Exception:
         except_handler_factory(config, qs=query_string.decode(config['charset']))()
+
+
+# adapted from cgitb.Hook
+def except_handler_factory(config: SectionProxy, out: Callable[[str], None] = None,
+                           qs: str = None) -> Callable[..., None]:
+    """
+    Log an exception gracefully.
+
+    config is a config object; out is a function that takes a string; qs is a bytes query string.
+    """
+    if not out:
+        out = sys.stdout.write
+    error_template = "<p class='error'>%s</p>"
+
+    def except_handler(etype=None, evalue=None, etb=None): # type: ignore
+        """
+        Log uncaught exceptions and display a friendly error.
+        """
+        if not etype or not evalue or not etb:
+            etype, evalue, etb = sys.exc_info()
+        import cgitb
+        out(cgitb.reset())
+        if not config.get("exception_dir", ""):
+            out(error_template % """
+    A problem has occurred, but it probably isn't your fault.
+    """)
+        else:
+            import stat
+            import traceback
+            if qs:
+                doc = "<h3><code>%s</code></h3>" % qs.decode('utf-8', 'replace')
+            try:
+                doc += cgitb.html((etype, evalue, etb), 5)
+            except:                  # just in case something goes wrong
+                doc += "<pre>" + ''.join(traceback.format_exception(etype, evalue, etb)) + "</pre>"
+            if config.getboolean('debug'):
+                out(doc)
+                return
+            try:
+                while etb.tb_next is not None:
+                    etb = etb.tb_next
+                e_file = etb.tb_frame.f_code.co_filename
+                e_line = etb.tb_frame.f_lineno
+                ldir = os.path.join(config['exception_dir'], os.path.split(e_file)[-1])
+                if not os.path.exists(ldir):
+                    os.umask(0o002)
+                    os.makedirs(ldir)
+                (fd, path) = tempfile.mkstemp(prefix="%s_" % e_line, suffix='.html', dir=ldir)
+                fh = os.fdopen(fd, 'w')
+                fh.write(doc)
+                fh.close()
+                os.chmod(path, stat.S_IROTH)
+                out(error_template % """\
+A problem has occurred, but it probably isn't your fault.
+REDbot has remembered it, and we'll try to fix it soon.""")
+            except:
+                out(error_template % """\
+A problem has occurred, but it probably isn't your fault.
+REDbot tried to save it, but it couldn't! Oops.<br>
+Please e-mail the information below to
+<a href='mailto:red@redbot.org'>red@redbot.org</a>
+and we'll look into it.""")
+                out("<h3>Original Error</h3>")
+                out("<pre>")
+                out(''.join(traceback.format_exception(etype, evalue, etb)))
+                out("</pre>")
+                out("<h3>Write Error</h3>")
+                out("<pre>")
+                out(''.join(traceback.format_exc()))
+                out("</pre>")
+        sys.exit(1) # We're in an uncertain state, so we must die horribly.
+
+    return except_handler
 
 
 if __name__ == "__main__":
