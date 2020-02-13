fe2b422d8447473f4dd90aaedccaf6e6ce958e75
Mark Nottingham <mnot@mnot.net>
2020-02-13 16:01:27 +0000

    tidy

--- bin/redbot_cgi.py
+++ bin/redbot_cgi.py
@@ -17,11 +17,12 @@ from redbot import __version__
 from redbot.type import RawHeaderListType, HttpResponseExchange
 from redbot.webui import RedWebUi
 
-_loop.precision = .1
+_loop.precision = 0.1
 
 
 def cgi_main(config: SectionProxy) -> None:
     """Run REDbot as a CGI Script."""
+
     def out(inbytes: bytes) -> None:
         try:
             sys.stdout.buffer.write(inbytes)
@@ -29,21 +30,24 @@ def cgi_main(config: SectionProxy) -> None:
         except IOError:
             pass
 
-    config['ui_uri'] = "%s://%s%s%s" % (
-        'HTTPS' in os.environ and "https" or "http",
-        os.environ.get('HTTP_HOST'),
-        os.environ.get('SCRIPT_NAME'),
-        os.environ.get('PATH_INFO', ''))
-    method = os.environ.get('REQUEST_METHOD').encode(config['charset'])
-    query_string = os.environ.get('QUERY_STRING', "").encode(config['charset'])
-    req_hdrs = [] # type: RawHeaderListType
-    for (k,v) in os.environ:
-        if k[:5] == 'HTTP_':
-            req_hdrs.append((k[:5].lower().encode('ascii'), v.encode('ascii')))
+    config["ui_uri"] = "%s://%s%s%s" % (
+        "HTTPS" in os.environ and "https" or "http",
+        os.environ.get("HTTP_HOST"),
+        os.environ.get("SCRIPT_NAME"),
+        os.environ.get("PATH_INFO", ""),
+    )
+    method = os.environ.get("REQUEST_METHOD").encode(config["charset"])
+    query_string = os.environ.get("QUERY_STRING", "").encode(config["charset"])
+    req_hdrs = []  # type: RawHeaderListType
+    for (k, v) in os.environ:
+        if k[:5] == "HTTP_":
+            req_hdrs.append((k[:5].lower().encode("ascii"), v.encode("ascii")))
 
     class Exchange(HttpResponseExchange):
         @staticmethod
-        def response_start(code: bytes, phrase: bytes, res_hdrs: RawHeaderListType) -> None:
+        def response_start(
+            code: bytes, phrase: bytes, res_hdrs: RawHeaderListType
+        ) -> None:
             out_v = [b"Status: %s %s" % (code, phrase)]
             for name, value in res_hdrs:
                 out_v.append(b"%s: %s" % (name, value))
@@ -67,16 +71,18 @@ def cgi_main(config: SectionProxy) -> None:
             thor.schedule(0, thor.stop)
 
     try:
-        RedWebUi(config, method.decode(config['charset']), query_string, req_hdrs,
-                 Exchange())
+        RedWebUi(
+            config, method.decode(config["charset"]), query_string, req_hdrs, Exchange()
+        )
         thor.run()
     except Exception:
-        except_handler_factory(config, qs=query_string.decode(config['charset']))()
+        except_handler_factory(config, qs=query_string.decode(config["charset"]))()
 
 
 # adapted from cgitb.Hook
-def except_handler_factory(config: SectionProxy, out: Callable[[str], int] = None,
-                           qs: str = None) -> Callable[..., None]:
+def except_handler_factory(
+    config: SectionProxy, out: Callable[[str], int] = None, qs: str = None
+) -> Callable[..., None]:
     """
     Log an exception gracefully.
 
@@ -86,28 +92,37 @@ def except_handler_factory(config: SectionProxy, out: Callable[[str], int] = Non
         out = sys.stdout.write
     error_template = "<p class='error'>%s</p>"
 
-    def except_handler(etype=None, evalue=None, etb=None): # type: ignore
+    def except_handler(etype=None, evalue=None, etb=None):  # type: ignore
         """
         Log uncaught exceptions and display a friendly error.
         """
         if not etype or not evalue or not etb:
             etype, evalue, etb = sys.exc_info()
         import cgitb
+
         out(cgitb.reset())
         if not config.get("exception_dir", ""):
-            out(error_template % """
+            out(
+                error_template
+                % """
     A problem has occurred, but it probably isn't your fault.
-    """)
+    """
+            )
         else:
             import stat
             import traceback
+
             if qs:
-                doc = "<h3><code>%s</code></h3>" % qs.decode('utf-8', 'replace')
+                doc = "<h3><code>%s</code></h3>" % qs.decode("utf-8", "replace")
             try:
                 doc += cgitb.html((etype, evalue, etb), 5)
-            except:                  # just in case something goes wrong
-                doc += "<pre>" + ''.join(traceback.format_exception(etype, evalue, etb)) + "</pre>"
-            if config.getboolean('debug'):
+            except:  # just in case something goes wrong
+                doc += (
+                    "<pre>"
+                    + "".join(traceback.format_exception(etype, evalue, etb))
+                    + "</pre>"
+                )
+            if config.getboolean("debug"):
                 out(doc)
                 return
             try:
@@ -115,47 +130,55 @@ def except_handler_factory(config: SectionProxy, out: Callable[[str], int] = Non
                     etb = etb.tb_next
                 e_file = etb.tb_frame.f_code.co_filename
                 e_line = etb.tb_frame.f_lineno
-                ldir = os.path.join(config['exception_dir'], os.path.split(e_file)[-1])
+                ldir = os.path.join(config["exception_dir"], os.path.split(e_file)[-1])
                 if not os.path.exists(ldir):
                     os.umask(0o002)
                     os.makedirs(ldir)
-                (fd, path) = tempfile.mkstemp(prefix="%s_" % e_line, suffix='.html', dir=ldir)
-                fh = os.fdopen(fd, 'w')
+                (fd, path) = tempfile.mkstemp(
+                    prefix="%s_" % e_line, suffix=".html", dir=ldir
+                )
+                fh = os.fdopen(fd, "w")
                 fh.write(doc)
                 fh.close()
                 os.chmod(path, stat.S_IROTH)
-                out(error_template % """\
+                out(
+                    error_template
+                    % """\
 A problem has occurred, but it probably isn't your fault.
-REDbot has remembered it, and we'll try to fix it soon.""")
+REDbot has remembered it, and we'll try to fix it soon."""
+                )
             except:
-                out(error_template % """\
+                out(
+                    error_template
+                    % """\
 A problem has occurred, but it probably isn't your fault.
 REDbot tried to save it, but it couldn't! Oops.<br>
 Please e-mail the information below to
 <a href='mailto:red@redbot.org'>red@redbot.org</a>
-and we'll look into it.""")
+and we'll look into it."""
+                )
                 out("<h3>Original Error</h3>")
                 out("<pre>")
-                out(''.join(traceback.format_exception(etype, evalue, etb)))
+                out("".join(traceback.format_exception(etype, evalue, etb)))
                 out("</pre>")
                 out("<h3>Write Error</h3>")
                 out("<pre>")
-                out(''.join(traceback.format_exc()))
+                out("".join(traceback.format_exc()))
                 out("</pre>")
-        sys.exit(1) # We're in an uncertain state, so we must die horribly.
+        sys.exit(1)  # We're in an uncertain state, so we must die horribly.
 
     return except_handler
 
 
 if __name__ == "__main__":
     conf = ConfigParser()
-    conf.read(os.environ.get('REDBOT_CONFIG', "config.txt"))
-    redconf = conf['redbot']
+    conf.read(os.environ.get("REDBOT_CONFIG", "config.txt"))
+    redconf = conf["redbot"]
 
     try:
-        locale.setlocale(locale.LC_ALL, locale.normalize(redconf['lang']))
+        locale.setlocale(locale.LC_ALL, locale.normalize(redconf["lang"]))
     except ValueError:
-        locale.setlocale(locale.LC_ALL, '')
+        locale.setlocale(locale.LC_ALL, "")
 
-    if 'GATEWAY_INTERFACE' in os.environ:  # CGI
+    if "GATEWAY_INTERFACE" in os.environ:  # CGI
         cgi_main(redconf)
--- bin/redbot_daemon.py
+++ bin/redbot_daemon.py
@@ -22,34 +22,34 @@ from redbot import __version__
 from redbot.type import RawHeaderListType
 from redbot.webui import RedWebUi
 
-_loop.precision = .1
+_loop.precision = 0.1
 
 
 class RedBotServer:
     """Run REDbot as a standalone Web server."""
 
     static_types = {
-        b'.html': b'text/html',
-        b'.js': b'text/javascript',
-        b'.css': b'text/css',
-        b'.png': b'image/png',
-        b'.txt': b'text/plain'
+        b".html": b"text/html",
+        b".js": b"text/javascript",
+        b".css": b"text/css",
+        b".png": b"image/png",
+        b".txt": b"text/plain",
     }
     watchdog_freq = 5
 
     def __init__(self, config: SectionProxy) -> None:
         self.config = config
-        self.static_files = {}    # type: Dict[bytes, bytes]
+        self.static_files = {}  # type: Dict[bytes, bytes]
         # Load static files
-        self.walk_files(config['asset_dir'], b"static/")
-        if config.get('extra_base_dir'):
-            self.walk_files(config['extra_base_dir'])
+        self.walk_files(config["asset_dir"], b"static/")
+        if config.get("extra_base_dir"):
+            self.walk_files(config["extra_base_dir"])
         # Set up the watchdog
         if os.environ.get("SYSTEMD_WATCHDOG"):
             thor.schedule(self.watchdog_freq, self.watchdog_ping)
         # Set up the server
-        server = thor.http.HttpServer(config.get('host', ''), int(config['port']))
-        server.on('exchange', self.red_handler)
+        server = thor.http.HttpServer(config.get("host", ""), int(config["port"]))
+        server.on("exchange", self.red_handler)
         try:
             thor.run()
         except KeyboardInterrupt:
@@ -61,49 +61,67 @@ class RedBotServer:
             for name in files:
                 try:
                     path = os.path.join(root, name)
-                    uri = os.path.relpath(path, dir_name).encode('utf-8')
-                    self.static_files[b"/%s%s" % (base, uri)] = open(path, 'rb').read()
+                    uri = os.path.relpath(path, dir_name).encode("utf-8")
+                    self.static_files[b"/%s%s" % (base, uri)] = open(path, "rb").read()
                     if uri.endswith(b"/index.html"):
-                        self.static_files[b"/%s%s" % (base, uri[:-10])] = open(path, 'rb').read()
+                        self.static_files[b"/%s%s" % (base, uri[:-10])] = open(
+                            path, "rb"
+                        ).read()
                 except IOError:
                     sys.stderr.write("* Problem loading %s\n" % path)
 
     def red_handler(self, x: EventEmitter) -> None:
         @thor.events.on(x)
-        def request_start(method: bytes, uri: bytes, req_hdrs: RawHeaderListType) -> None:
+        def request_start(
+            method: bytes, uri: bytes, req_hdrs: RawHeaderListType
+        ) -> None:
             p_uri = urlsplit(uri)
             if p_uri.path in self.static_files:
-                file_ext = os.path.splitext(p_uri.path)[1].lower() or b'.html'
-                content_type = self.static_types.get(file_ext, b'application/octet-stream')
+                file_ext = os.path.splitext(p_uri.path)[1].lower() or b".html"
+                content_type = self.static_types.get(
+                    file_ext, b"application/octet-stream"
+                )
                 headers = []
-                headers.append((b'Content-Type', content_type))
-                headers.append((b'Cache-Control', b'max-age=3600'))
+                headers.append((b"Content-Type", content_type))
+                headers.append((b"Cache-Control", b"max-age=3600"))
                 x.response_start(b"200", b"OK", headers)
                 x.response_body(self.static_files[p_uri.path])
                 x.response_done([])
             elif p_uri.path == b"/":
                 try:
                     req_hdrs.append(
-                      (b'client-ip', x.http_conn.tcp_conn.socket.getpeername()[0].encode('idna')))
-                    RedWebUi(self.config,
-                             method.decode(self.config['charset']), p_uri.query, req_hdrs,
-                             x, self.error_log)
+                        (
+                            b"client-ip",
+                            x.http_conn.tcp_conn.socket.getpeername()[0].encode("idna"),
+                        )
+                    )
+                    RedWebUi(
+                        self.config,
+                        method.decode(self.config["charset"]),
+                        p_uri.query,
+                        req_hdrs,
+                        x,
+                        self.error_log,
+                    )
                 except Exception:
-                    self.error_log("""
+                    self.error_log(
+                        """
 
 *** FATAL ERROR
 REDbot has encountered a fatal error which it really, really can't recover from
 in standalone server mode. Details follow.
-""")
+"""
+                    )
                     import traceback
+
                     dump = traceback.format_exc()
                     thor.stop()
                     self.error_log(dump)
                     sys.exit(1)
             else:
                 headers = []
-                headers.append((b'Content-Type', b'text/plain'))
-                headers.append((b'Cache-Control', b'max-age=3600'))
+                headers.append((b"Content-Type", b"text/plain"))
+                headers.append((b"Cache-Control", b"max-age=3600"))
                 x.response_start(b"404", b"Not Found", headers)
                 x.response_body(b"'%s' not found." % p_uri.path)
                 x.response_done([])
@@ -116,10 +134,10 @@ in standalone server mode. Details follow.
         thor.schedule(self.watchdog_freq, self.watchdog_ping)
 
 
-
 if __name__ == "__main__":
 
     from optparse import OptionParser
+
     usage = "Usage: %prog configfile"
     version = "REDbot version %s" % __version__
     option_parser = OptionParser(usage=usage, version=version)
@@ -129,15 +147,16 @@ if __name__ == "__main__":
 
     conf = ConfigParser()
     conf.read(args[0])
-    redconf = conf['redbot']
+    redconf = conf["redbot"]
 
     try:
-        locale.setlocale(locale.LC_ALL, locale.normalize(redconf['lang']))
+        locale.setlocale(locale.LC_ALL, locale.normalize(redconf["lang"]))
     except ValueError:
-        locale.setlocale(locale.LC_ALL, '')
+        locale.setlocale(locale.LC_ALL, "")
 
     sys.stderr.write(
-        "Starting on PID %s...\n" % os.getpid() + \
-        "http://%s:%s/\n" % (redconf.get('host', ''), redconf['port']))
+        "Starting on PID %s...\n" % os.getpid()
+        + "http://%s:%s/\n" % (redconf.get("host", ""), redconf["port"])
+    )
 
     RedBotServer(redconf)
