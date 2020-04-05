3dd9192f0d105b1c402ab4a0704291a729af858f
Mark Nottingham <mnot@mnot.net>
2020-04-05 15:20:25 +1000

    update Warning
    Fixes #58

--- redbot/message/headers/warning.py
+++ redbot/message/headers/warning.py
@@ -8,12 +8,10 @@ class warning(headers.HttpHeader):
     canonical_name = "Warning"
     description = """\
 The `Warning` header is used to carry additional information about the status or transformation of
-a message that might not be reflected in it. This information is typically used to warn about
-possible incorrectness introduced by caching operations or transformations applied to the body of
-the message."""
+a message that might not be reflected in it. It has been deprecated."""
     reference = "%s#header.warning" % rfc7234.SPEC_URL
     syntax = rfc7234.Warning
     list_header = True
-    deprecated = False
+    deprecated = True
     valid_in_requests = False
     valid_in_responses = True
