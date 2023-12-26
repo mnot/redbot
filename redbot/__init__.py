d30e3d0e3c231c281c346f4a0b29a0681ecd1137
Mark Nottingham <mnot@mnot.net>
2023-12-27 09:07:42 +1100

    bump version

--- redbot/__init__.py
+++ redbot/__init__.py
@@ -2,7 +2,7 @@
 This is the Resource Expert Droid.
 """
 
-__version__ = "2.0.9"
+__version__ = "2.0.10"
 __author__ = "Mark Nottingham <mnot@mnot.net>"
 __copyright__ = """\
 Copyright (c) 2008- Mark Nottingham
