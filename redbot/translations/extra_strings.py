c866eb6fc1990dcc30e0c4fb67e207d488b15ae7
Mark Nottingham <mnot@mnot.net>
2025-11-28 18:56:25 +1100

    Added: French translation

---
+++ redbot/translations/extra_strings.py
@@ -0,0 +1,14 @@
+from redbot.i18n import _
+
+# These are extra strings from httplint that need to be translated
+
+
+def extra_strings() -> None:
+    # httplint categories
+    _("General")
+    _("Browser Security")
+    _("Content Negotiation")
+    _("Caching")
+    _("Validation")
+    _("Connection")
+    _("Partial Content")
