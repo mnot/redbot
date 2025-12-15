0f33d9f14125a5e416d404f47743d5589373d001
Mark Nottingham <mnot@mnot.net>
2025-12-16 09:22:43 +1100

    Changed: use a local test server

--- test/test_webui.py
+++ test/test_webui.py
@@ -2,14 +2,65 @@
 # coding=UTF-8
 
 from playwright.sync_api import sync_playwright, TimeoutError
-
+import http.server
+import threading
 import time
 import unittest
+import sys
+from multiprocessing import Process
+
 
 TIMEOUT = 30 * 1000
 
+class TestHandler(http.server.BaseHTTPRequestHandler):
+    protocol_version = "HTTP/1.1"
+
+    def prepare_response(self):
+        if self.path == "/img.png":
+            content = b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\nIDATx\x9cc\x00\x01\x00\x00\x05\x00\x01\r\n-\xb4\x00\x00\x00\x00IEND\xaeB`\x82"
+            ctype = "image/png"
+        else:
+            content = b"<html><body><h1>Hello World</h1><img src='img.png'></body></html>"
+            ctype = "text/html"
+
+        self.send_response(200)
+        self.send_header("Content-type", ctype)
+        self.send_header("Content-Length", str(len(content)))
+        self.send_header("Cache-Control", "max-age=3600")
+        self.send_header("Connection", "close")
+        self.end_headers()
+        return content
+
+    def do_GET(self):
+        content = self.prepare_response()
+        self.wfile.write(content)
+        self.wfile.flush()
+        self.close_connection = True
+
+    def do_HEAD(self):
+        self.prepare_response()
+        self.wfile.flush()
+        self.close_connection = True
+
+
+class TestServer(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.port = 8001  # Different from redbot port
+        self.server = http.server.ThreadingHTTPServer(('127.0.0.1', self.port), TestHandler)
+        self.server.allow_reuse_address = True
+        self.daemon = True
+
+    def run(self):
+        self.server.serve_forever()
+
+    def stop(self):
+        self.server.shutdown()
+        self.server.server_close()
+
+
 class BasicWebUiTest(unittest.TestCase):
-    test_uri = "https://www.mnot.net/"
+    test_uri = "http://127.0.0.1:8001/"
 
     def setUp(self):
         self.page = browser.new_page()
@@ -21,7 +72,7 @@ class BasicWebUiTest(unittest.TestCase):
     def test_multi(self):
         self.page.click('input[value="check embedded"]')
         self.check_complete()
-
+        
     def check_complete(self):
         try:
             self.page.wait_for_selector("div.footer", timeout=TIMEOUT)
@@ -30,16 +81,13 @@ class BasicWebUiTest(unittest.TestCase):
             raise AssertionError("Timeout")
 
 
-#class CnnWebUiTest(BasicWebUiTest):
-#    test_uri = "https://edition.cnn.com/"
-
-
 def redbot_run():
     import redbot.daemon
     from configparser import ConfigParser
 
     conf = ConfigParser()
     conf.read("config.txt")
+    conf["redbot"]["enable_local_access"] = "true"
     redconf = conf["redbot"]
     server = redbot.daemon.RedBotServer(redconf)
     server.run()
@@ -49,18 +97,25 @@ if __name__ == "__main__":
     test_host = "localhost"
     test_port = 8000
     redbot_uri = "http://%s:%s/" % (test_host, test_port)
-    import sys
-
+    
     sys.path.insert(0, "bin")
-    from multiprocessing import Process
 
+    # Start REDbot server
     p = Process(target=redbot_run)
     p.start()
-    with sync_playwright() as pw:
-        browser = pw.chromium.launch()
-        tests = unittest.main(exit=False, verbosity=2)
-        browser.close()
-    print("done webui test...")
-    p.terminate()
-    if len(tests.result.errors) > 0 or len(tests.result.failures) > 0:
-        sys.exit(1)
+    
+    # Start local test server
+    test_server = TestServer()
+    test_server.start()
+    
+    try:
+        with sync_playwright() as pw:
+            browser = pw.chromium.launch()
+            tests = unittest.main(exit=False, verbosity=2)
+            browser.close()
+            print("done webui test...")
+    finally:
+        p.terminate()
+        # test_server.stop() # daemon thread dies with main
+        if 'tests' in locals() and (len(tests.result.errors) > 0 or len(tests.result.failures) > 0):
+            sys.exit(1)
