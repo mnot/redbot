64e43e0f948f649921e14316d4e118a0974285c8
Mark Nottingham <mnot@mnot.net>
2026-04-08 15:41:42 +1000

    Changed: more detailed formatting errors

--- redbot/note.py
+++ redbot/note.py
@@ -1,6 +1,6 @@
 from httplint.note import Note
 from markupsafe import Markup, escape
-from redbot.i18n import _
+from redbot.i18n import _, get_locale
 
 
 class RedbotNote(Note):
@@ -9,14 +9,26 @@ class RedbotNote(Note):
     """
 
     def _get_summary(self) -> Markup:
-        return Markup(_(self._summary) % self.vars)
+        try:
+            return Markup(_(self._summary) % self.vars)
+        except TypeError as err:
+            raise TypeError(
+                f"Summary formatting error in {self.__class__.__name__} "
+                f"(locale: {get_locale()}): {err} (vars: {self.vars!r})"
+            ) from err
 
     def _get_detail(self) -> Markup:
-        return Markup(
-            self._markdown.reset().convert(
-                _(self._text) % {k: escape(str(v)) for k, v in self.vars.items()}
+        try:
+            return Markup(
+                self._markdown.reset().convert(
+                    _(self._text) % {k: escape(str(v)) for k, v in self.vars.items()}
+                )
             )
-        )
+        except TypeError as err:
+            raise TypeError(
+                f"Detail formatting error in {self.__class__.__name__} "
+                f"(locale: {get_locale()}): {err} (vars: {self.vars!r})"
+            ) from err
 
     summary = property(_get_summary)
     detail = property(_get_detail)
